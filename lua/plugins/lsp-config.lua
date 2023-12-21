331ce2675d9faca3c1dcf8b7d6731d647db81150
Algirdas Beresnevičius <damnaz@gmail.com>
2023-12-21 23:56:16 +0200

    telescope-select-ui

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,10 +33,13 @@ return {
       local lspconfig = require('lspconfig')
       lspconfig.lua_ls.setup({})
       lspconfig.csharp_ls.setup({})
+      lspconfig.intelephense.setup({})
+
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
