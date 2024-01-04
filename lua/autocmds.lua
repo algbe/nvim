fccd131d11d732f4b65d6a574fa507f86b10901a
Algirdas Beresnevičius <damnaz@gmail.com>
2024-01-04 07:27:56 +0200

    Copilot + Telescope fix

---
+++ lua/autocmds.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
