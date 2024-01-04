8b15e5ca3739ebe7d761cddd36081e03d69a52b2
Algirdas Beresnevičius <damnaz@gmail.com>
2024-01-04 07:26:36 +0200

    Copilot and Telescope insert mode fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,7 +30,6 @@ return {
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
 }
