eed967ca5de7abc5d54a140af3f44c565a901990
Algirdas B <damnaz@gmail.com>
2024-04-05 00:29:03 +0300

    Missed file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,15 +1,16 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			enable_normal_mode_for_inputs = true,
-		})
-		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left git_status<CR>", {})
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            enable_normal_mode_for_inputs = true,
+        })
+        vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+        vim.keymap.set("n", "<leader>g", ":Neotree float git_status<CR>", {})
+    end,
 }
