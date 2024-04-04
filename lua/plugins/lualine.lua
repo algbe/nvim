26963737cc5608f0035d6e36b4136d9481c6434a
Algirdas B <damnaz@gmail.com>
2024-04-05 00:48:33 +0300

    lualine update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,27 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			theme = "tokyonight",
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        local lazy_status = require("lazy.status")
+
+        require("lualine").setup({
+            theme = "tokyonight",
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+                lualine_c = { "filename" },
+                lualine_x = {
+                    {
+                        lazy_status.updates,
+                        cond = lazy_status.has_updates,
+                        color = { fg = "#f7768e" },
+                    },
+                    "encoding",
+                    "fileformat",
+                    "filetype",
+                },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+        })
+    end,
 }
